11108c1f5ae04a12f1ae4c728715e4be6cda76bb
Urvashi2409 <urvashicode2410@gmail.com>
2023-07-15 15:41:55 +0530

    2 classes of js

---
+++ script.js
@@ -0,0 +1,46 @@
+console.log("Hello, Daniel");
+
+// literals and variables 
+
+// literals 
+// 23.4 
+// "Daniel"
+
+// variables
+// a = "Urvashi";
+// b = 4;
+
+// literals: It cannot be changed 
+// variables: It can be changed 
+
+// document.querySelector('#heading2').textContent = "yes";
+
+let name = "Daniel";
+console.log(name);
+
+let age = 23;
+console.log(age);
+
+age = 45;
+console.log(age);
+
+// if we use let then we can modify the value of the variable 
+
+const last = "Guba";
+console.log(last);
+
+// last = "Anand";
+// console.log(last);
+
+// you can change the value of let variable but not the const 
+
+var gender = "male";
+console.log(gender);
+
+gender = "female";
+console.log(gender);
+
+// naming conventions for a variable:
+
+let YEERu = "you";
+
