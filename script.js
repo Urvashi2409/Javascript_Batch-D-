dd77dbb45a55ad6e753c83c29c2ebc89c10cf37b
Urvashi2409 <urvashicode2410@gmail.com>
2023-07-20 06:44:04 +0530

    data types, arithematic operators

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log("Hello, Daniel");
+// console.log("Hello, Daniel");
 
 // literals and variables 
 
@@ -15,32 +15,80 @@ console.log("Hello, Daniel");
 
 // document.querySelector('#heading2').textContent = "yes";
 
-let name = "Daniel";
-console.log(name);
+// let name = "Daniel";
+// console.log(name);
 
-let age = 23;
-console.log(age);
+// let age = 23;
+// console.log(age);
 
-age = 45;
-console.log(age);
+// age = 45;
+// console.log(age);
 
 // if we use let then we can modify the value of the variable 
 
-const last = "Guba";
-console.log(last);
+// const last = "Guba";
+// console.log(last);
 
 // last = "Anand";
 // console.log(last);
 
 // you can change the value of let variable but not the const 
 
-var gender = "male";
-console.log(gender);
+// var gender = "male";
+// console.log(gender);
 
-gender = "female";
-console.log(gender);
+// gender = "female";
+// console.log(gender);
 
 // naming conventions for a variable:
 
-let YEERu = "you";
+// let YEERu = "you";
 
+// data types in javascript 
+
+// every number even if its an integer or a decimal, comes under number data type 
+// let a = 34;
+// console.log(typeof a);
+
+// let a = 45.56;
+// console.log(typeof a);
+
+// let a = 34.3434423446942334623963636949634;
+// console.log(typeof a);
+
+// a single character and multiple characters, comes under the string data type 
+// let a = 'r';
+// console.log(typeof a);
+
+// let a = "Hello, world!";
+// console.log(typeof a);
+
+// let a = false;
+// console.log(typeof a);
+
+// if no value is there = undefined 
+// let a;
+// console.log(typeof a);
+
+// if empty value is there, then null 
+// let a = null;
+// console.log(typeof a);
+
+// if you want to show a big integer then put a n at the end of the number 
+// let a = 2424484923462342496823948626394n;
+// console.log(typeof a);
+
+// Arithematic operators 
+// console.log(3+4);
+// console.log(3-4);
+// console.log(3*4);
+// console.log(3/4);
+
+// in modulo operation, 
+// if dividend < divisor, then ans = dividend 
+// if dividend > divisor, then ans = remainder 
+// console.log(11%3);
+// console.log(3%4);
+
+// in javascript, exponentiation symbol is ** 
+// console.log(2**3);
