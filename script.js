3810d44333fb112c4061063b18c656d44836d65f
Urvashi2409 <urvashicode2410@gmail.com>
2023-08-27 21:32:20 +0530

    string scling

--- script.js
+++ script.js
@@ -195,36 +195,36 @@
 //     console.log("Default value!!");
 // }
 
-x = 12;
-y = 3;
-z = 11;
-if(x == 2 && x < 3){
-    console.log(x);
-}
-if(x != 5){
-    console.log("whatever");
-}
-if(x != 5 && y >= 5){
-    console.log("its gonna happen");
-}
-if(z != 0 || x == 2){
-    console.log("you say");
-}
-if(!(y < 10)){
-    console.log(y);
-}
-else if(x < 10 || x < 5){
-    console.log("okkkkkkk");
-}
-else if(y < 10 || y <= 0){
-    console.log("pikachu");
-}
-else if(z == 0 && y ==5){
-    console.log("Pikaboo");
-}
-else{
-    console.log("Default value");
-}
+// x = 12;
+// y = 3;
+// z = 11;
+// if(x == 2 && x < 3){
+//     console.log(x);
+// }
+// if(x != 5){
+//     console.log("whatever");
+// }
+// if(x != 5 && y >= 5){
+//     console.log("its gonna happen");
+// }
+// if(z != 0 || x == 2){
+//     console.log("you say");
+// }
+// if(!(y < 10)){
+//     console.log(y);
+// }
+// else if(x < 10 || x < 5){
+//     console.log("okkkkkkk");
+// }
+// else if(y < 10 || y <= 0){
+//     console.log("pikachu");
+// }
+// else if(z == 0 && y ==5){
+//     console.log("Pikaboo");
+// }
+// else{
+//     console.log("Default value");
+// }
 
 
 // x = 2;
@@ -258,3 +258,198 @@ else{
 //     console.log("Default value");
 // }
 
+// Bitwise operators 
+
+// console.log(4&2);
+// console.log(13&10);
+// console.log(20&30);
+
+// console.log(7&3);
+// console.log(128&71);
+
+// console.log(58|34);
+// console.log(~(7&3));
+// console.log(~(12&8));
+
+// console.log(12^4);
+// console.log(4^2);
+
+// Arithematic operators = %, -, +, /, *, ** 
+// logical operators = &&, ||, ! 
+// Bitwise operators = &, |, ^, ~ 
+// Relational operators = >, <, ==, !=, >=, <= 
+// Assignment operator = =, +=, -=, *=, /=, %=, **= 
+
+// Number, String, null, undefined, boolean,bigInt 
+
+// Variable naming conventions
+// no num at start 
+// $, _ are only allowed symbols 
+// uppercase and lowercase allowed  
+// spaces are not allowed  
+
+// let aa bb = 45; 
+
+// functions 
+// function greeting(name){
+//     console.log("Hi", name);
+//     console.log("You also got an F");
+//     console.log("Failing is a part of life");
+//     console.log("You study more");
+//     console.log("Eat more chocolates!!!");
+//     console.log("You didn't get all A+ Terrible!");
+//     console.log("You didn't fail everything");
+// }
+
+// greeting("Ethan");
+// greeting("Celina");
+// greeting("John");
+// greeting("Angelina");
+
+// function definition 
+// a, b = parameters 
+// function add(a, b){
+//     c = a + b 
+//     console.log(c)
+// }
+
+// function call 
+// (5, 5) = arguments 
+// add(512, 5)
+
+// function func(x){
+//     x = 23
+//     x += 2 
+//     return x 
+// }
+
+// ans = func(2)
+// console.log(ans)
+
+
+
+
+// 23 2 1 2 23
+
+
+// global scope 
+// x = 23 
+// console.log(x)
+
+// function scope 
+// function func(x){
+//     console.log(x)
+//     x--
+//     console.log(x)
+//     x++;
+//     console.log(x)
+//     x += 21
+//     return x 
+// }
+
+// x = func(2)
+// console.log(x)
+
+// 12 23 12 
+
+// global scope 
+// x = 12
+// console.log(x)
+// function func(x){
+    // function scoped 
+//     x = 23
+//     console.log(x)
+// }
+
+// func(x)
+// console.log(x)
+
+// 2 
+
+// if we try to print function's variable a outside the function it will give error 
+// function func(a){
+//     console.log(a)
+// }
+
+// func(2)
+// console.log(a)
+
+// Justin = "I am the famous one"
+// console.log(Justin) // "I am the famous one"
+// function smallTown(){
+//     Justin = "I am small town one"
+//     console.log(Justin) // "I am small town one"
+// }
+
+// console.log(Justin) // I am the famous one
+// smallTown()
+// console.log(Justin) // I am small town one
+
+// strings  
+// mystr = "Raymond"
+// newstr = "Raymond"
+
+// if (mystr == newstr){
+//     console.log("Equal")
+// }
+// else{
+//     console.log("not equal")
+// }
+
+
+// if (mystr === newstr){
+//     console.log("Equal")
+// }
+// else{
+//     console.log("not equal")
+// }
+
+// mystr = "45"
+// mynum = 45 
+
+// if (mystr === mynum){
+//     console.log("Equal")
+// }
+// else{
+//     console.log("not equal")
+// }
+
+// == will return true if both values are equal even if both data types are unequal
+// === will return true if values and data types both of the things are equal
+
+// slicing in strings 
+
+// let str1 = "I am a string. I am so ugly"
+// console.log(str1.slice(0, 9))
+
+// let str = "Daniel lives in los Angeles"
+// console.log(str.slice(3, 8))
+
+// we include the starting index value but not the ending index value
+// let str = "Daniel lives in los Angeles"
+// console.log(str.slice(2, 5))
+
+// let str = "Daniel lives in los Angeles"
+console.log(str.slice(-11, -8))
+console.log(str.slice(-11, 19))
+console.log(str.slice(16, -8))
+console.log(str.slice(16, 19))
+
+console.log(str.slice(13, 27))
+console.log(str.slice(-14))
+console.log(str.slice(13))
+console.log(str.slice(-14, 27))
+
+// let str = "tampa bay buccaneers"
+
+console.log(str.slice(2, 9))
+console.log(str.slice(-18, -11))
+console.log(str.slice(2, -11))
+console.log(str.slice(-18, 9))
+
+console.log(str.slice(-13, -6))
+console.log(str.slice(-12))
+console.log(str.slice(-16, 5))
+console.log(str.slice(17, 19))
+console.log(str.slice(16, -3))
+console.log(str.slice(-1))
